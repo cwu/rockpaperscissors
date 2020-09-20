02e58f51cc020402abea2fedcab8e72428a3dc90
Glitch (cwu-rockpaperscissors) <none>
2020-09-20 23:36:40 +0000

    Added javascript to display choice, as well as some css styling

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@
 
 
 
+$('#shoot').click(function() {
+  const userChoice = $('#input').val();
+  
+  // Display user's choice
+  $('#userChoice').text(userChoice);
+  
+  // Select random computer choice
+  const allChoices = ['rock', 'paper', 'scissors'];
+  const randomChoiceIndex = Math.floor(Math.random() * allChoices.length);
+  const computerChoice = allChoices[randomChoiceIndex];
+  $('#computerChoice').text(computerChoice);
+});
