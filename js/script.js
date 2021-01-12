388a00464387b2f7fae9b01668809ac14775bf69
Glitch (chris-rps) <none>
2021-01-12 23:57:07 +0000

    🏅🌱 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -7,15 +7,3 @@
 
 
 
-$('#shoot').click(function() {
-  const userChoice = $('#input').val();
-  
-  // Display user's choice
-  $('#userChoice').text(userChoice);
-  
-  // Select random computer choice
-  const allChoices = ['rock', 'paper', 'scissors'];
-  const randomChoiceIndex = Math.floor(Math.random() * allChoices.length);
-  const computerChoice = allChoices[randomChoiceIndex];
-  $('#computerChoice').text(computerChoice);
-});
